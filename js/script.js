8e2cabbc808e86b9beb90e9afa039c54c2c6a075
Vitor Souza <vitorhugovhps@outlook.com>
2021-12-05 16:32:23 -0300

    DOM and calculations is working

--- js/script.js
+++ js/script.js
@@ -33,3 +33,69 @@ function operate(num1,num2,operator){
 
     return result;
 }
+
+let num1 = "";
+let num2 = "";
+let operator = "";
+
+
+const display = document.querySelector(".display");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        display.innerText += button.textContent;
+
+
+        if(button.textContent == "+"){
+            num1 = display.textContent;
+            num1 = num1.substring(0, num1.length - 1);
+            display.innerText = "+";
+            operator = display.textContent;
+
+        }else if (button.textContent == "-"){
+            num1 = display.textContent;
+            num1 = num1.substring(0, num1.length - 1);
+            display.innerText = "-";
+            operator = display.textContent;
+
+        }else if (button.textContent == "/"){
+            num1 = display.textContent;
+            num1 = num1.substring(0, num1.length - 1);
+            display.innerText = "/";
+            operator = display.textContent;
+
+        } else if (button.textContent == "*"){
+            num1 = display.textContent;
+            num1 = num1.substring(0, num1.length - 1);
+            display.innerText = "*";
+            operator = display.textContent;
+        }
+
+        if(button.textContent == "="){
+            num2 = display.textContent;
+            display.textContent = "";
+
+            num2 = num2.substring(1);
+            // num2 = num2.substring(0, num1.length - 1);
+
+            display.textContent = operate(parseInt(num1), parseInt(num2), operator);
+
+            num1 = display.textContent;
+
+            // console.log("num1 after the result = " + num1);
+        }
+
+        if(button.textContent == "CE"){
+            num1 = "";
+            num2 = "";
+            operator = "";
+            display.textContent = "";
+        }
+
+        // console.log("number 1: " + num1);
+        // console.log("operator: " + operator);
+        // console.log("number 2: " + num2);
+    });
+})
